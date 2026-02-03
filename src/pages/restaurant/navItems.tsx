ba5bfc02bcd3e060b62970645ebfb504807d3413
Alberto Paitoni Faustinoni <alberto.paitoni@gmail.com>
2026-02-03 16:09:01 +0100

    nascosti dashboard e menu

--- src/pages/restaurant/navItems.tsx
+++ src/pages/restaurant/navItems.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { LayoutDashboard, Calendar, Utensils, LayoutGrid } from 'lucide-react';
 
 export const restaurantNavItems = [
-    { label: 'Dashboard', icon: <LayoutDashboard size={20} />, path: '/restaurant/dashboard' },
+    //{ label: 'Dashboard', icon: <LayoutDashboard size={20} />, path: '/restaurant/dashboard' },
     { label: 'Prenotazioni', icon: <Calendar size={20} />, path: '/restaurant/reservations' },
     { label: 'Sale e Tavoli', icon: <LayoutGrid size={20} />, path: '/restaurant/rooms' },
-    { label: 'Menu', icon: <Utensils size={20} />, path: '/restaurant/menu' },
+    //{ label: 'Menu', icon: <Utensils size={20} />, path: '/restaurant/menu' },
 ];
 
